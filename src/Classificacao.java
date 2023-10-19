02902fff42f05d87a8004d5c27431e03bb680e1e
FBC-LTS <fabricioalcc@gmail.com>
2023-10-18 23:44:30 -0300

    Class Gasto & Classificação
    Criei as classes com os construtores e deixei comentários de como fazer os métodos!

--- src/Classificacao.java
+++ src/Classificacao.java
@@ -1,9 +1,29 @@
+package tiposPack;
+
 public class Classificacao {
     String nome;
-    int tipo;
-    double valor;
+    int codCategoria; // 0 = desembolso ; 1 = perda ; 2 = despesa ; 3 = investimento ; 4 = custo ; 
+    int subCategoria; // 0 = none ; 1 = comercial ; 2 = industrial ;
 
-    void classificarGasto() {
+    // Construtor com ambas
+    public Classificacao(int codCategoria, int subCategoria){
+        this.codCategoria = codCategoria; // 0 = desembolso ; 1 = perda ; 2 = despesa ; 3 = investimento ; 4 = custo ; 
+        this.subCategoria = subCategoria; // 0 = none ; 1 = comercial ; 2 = industrial ;
+        nomearClassifcacao();
+    }
+    // Construtor com valor padrão para subCategoria
+    public Classificacao(int codCategoria) {
+        this.codCategoria = codCategoria;
+        this.subCategoria = 0; // Valor padrão para subCategoria
+        nomearClassifcacao();
+    }
 
+    void nomearClassifcacao() {
+        System.out.println("nomeando.");
+        // nomear a string nome de acordo com os valores de codCategoria e subCategoria
+        if(this.codCategoria == 4){
+            System.out.println("subclassificando.");
+            // classicar o {nome} + {industrial ou comercial} dependendo do subCategoria
+        }
     }
 }
--- src/Gasto.java
+++ src/Gasto.java
@@ -1,9 +1,24 @@
+import tiposPack.Classificacao;
+
 public class Gasto {
     String nome;
-    int codCategoria;
-    int subCategoria;
+    double valor;
+    int tipo; // 0= passivo ; 1= ativo ; 
+    Classificacao classificacao;
 
-    private void classificarCategoria() {
-        
+    // contrutor com subcategoria
+    public void Gasto(String nome, double valor, int tipo, int codCategoria, int subCategoria){
+        this.nome = nome;
+        this.valor = valor;
+        this.tipo = tipo; // 0= passivo ; 1= ativo ; 
+        this.classificacao = new Classificacao(codCategoria, subCategoria);
     }
+    // construtor sem subcategoria
+    public void Gasto(String nome, double valor, int tipo, int codCategoria){
+        this.nome = nome;
+        this.valor = valor;
+        this.tipo = tipo; // 0= passivo ; 1= ativo ; 
+        this.classificacao = new Classificacao(codCategoria);
+    }
+
 }
