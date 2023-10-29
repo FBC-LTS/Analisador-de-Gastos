379c8008487aa42cb63fdf46c192256814ad1d88
Bruno Sousa <bknifepicarth@gmail.com>
2023-10-29 19:34:51 -0300

    tela home

--- src/main/java/com/example/application/views/home/HomeView.java
+++ src/main/java/com/example/application/views/home/HomeView.java
@@ -1,33 +1,36 @@
 package com.example.application.views.home;
 
 import com.example.application.views.MainLayout;
-import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 @PageTitle("Home")
 @Route(value = "", layout = MainLayout.class)
-public class HomeView extends HorizontalLayout {
-
-    private TextField name;
-    private Button sayHello;
-
+@StyleSheet("./estilos/home.css")
+public class HomeView extends HorizontalLayout{
     public HomeView() {
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
+        VerticalLayout container = new VerticalLayout();
+        container.addClassName("container");
+        container.getElement().getStyle().set("width", "50%");
+        container.getElement().getStyle().set("margin-left", "25%");
+        H1 titulo = new H1("Analisador de Gastos");
+        Paragraph textoAnalisador = new Paragraph("O Analisador de Gastos é uma aplicação intuitiva e eficiente projetada para simplificar o processo de registro, categorização e análise de despesas financeiras pessoais ou comerciais. Essa ferramenta versátil oferece recursos essenciais para auxiliar os usuários a manter o controle de seus gastos, categorizá-los e gerar relatórios detalhados em formato CSV.");
+        Paragraph textoInfo = new Paragraph("Para mais informações consulte a aba de documentação.");
+        Paragraph textoIniciar = new Paragraph("Clique em iniciar para começar a gerenciar seus gastos.");
+        Button iniciar = new Button("Iniciar");
+        iniciar.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        iniciar.addClickListener(e -> {
+            UI.getCurrent().navigate("Inicio");
         });
-        sayHello.addClickShortcut(Key.ENTER);
-
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
-
-        add(name, sayHello);
+        container.add(titulo, textoAnalisador, textoInfo, textoIniciar, iniciar);
+        add(container);
     }
-
 }
