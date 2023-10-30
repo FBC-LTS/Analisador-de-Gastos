6e11f9f7e20473459cdb236039b47c5b7b8067c1
FBC-LTS <fabricioalcc@gmail.com>
2023-10-30 01:02:38 -0300

    Criando formulario basico

--- src/main/java/com/example/application/views/gastos/GastosView.java
+++ src/main/java/com/example/application/views/gastos/GastosView.java
@@ -3,9 +3,12 @@ package com.example.application.views.gastos;
 import com.example.application.Analisador.Analise;
 import com.example.application.views.MainLayout;
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -42,15 +45,42 @@ public class GastosView extends VerticalLayout {
 
     private FormLayout formulario(FormLayout formLayout){
 
-        TextField gasto = new TextField("Nome:");
-        gasto.addClassName("input-form");
-        gasto.setClearButtonVisible(true);
-        gasto.setTooltipText("Insira o nome do gasto.");
+        TextField nomeGasto = new TextField("Nome:");
+        nomeGasto.addClassName("input-form");
+        nomeGasto.setClearButtonVisible(true);
+        nomeGasto.setTooltipText("Insira o nome do gasto.");
         
         ComboBox<String> tipo = new ComboBox<>("Tipo");
         tipo.setItems("ativo", "passivo");
 
-        formLayout.add(gasto,tipo);
+        ComboBox<String> classificacao = new ComboBox<>("Classificação");
+        classificacao.setItems("Perda", "Retornável", "Relacionado a produção?");
+
+        ComboBox<String> subClassificacao = new ComboBox<>("Sub-classificação:");
+        subClassificacao.setItems("Indiretamente", "Diretamente comercial", "Diretamente industrial");
+        subClassificacao.setVisible(false);
+        
+        classificacao.addValueChangeListener(event -> {
+            String selectedValue = event.getValue();
+            if ("Relacionado a produção?".equals(selectedValue)) {
+                subClassificacao.setVisible(true); // Torna a ComboBox subClassificacao visível
+            } else {
+                subClassificacao.setVisible(false); // Torna a ComboBox subClassificacao invisível
+            }
+        });
+
+        TextField preco = new TextField("Preço:");
+        preco.addClassName("input-form");
+        preco.setClearButtonVisible(true);
+        preco.setTooltipText("informe o preço/ valor do gasto em numero, com '.' separando as casas decimais");
+        preco.setPrefixComponent(new Span("R$:"));
+
+        Button salvar = new Button("Salvar");
+        salvar.addClassName("botao");
+        salvar.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+
+        formLayout.add(nomeGasto, tipo, classificacao, subClassificacao, preco, salvar);
         return formLayout;
     }
     
