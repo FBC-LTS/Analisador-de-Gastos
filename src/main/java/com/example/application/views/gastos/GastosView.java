6de4cb102d99d30344e2ebcb5d64e4a5cefc65b8
FBC-LTS <fabricioalcc@gmail.com>
2023-10-29 23:48:03 -0300

    preset

--- src/main/java/com/example/application/views/gastos/GastosView.java
+++ src/main/java/com/example/application/views/gastos/GastosView.java
@@ -3,9 +3,12 @@ package com.example.application.views.gastos;
 import com.example.application.Analisador.Analise;
 import com.example.application.views.MainLayout;
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -14,6 +17,7 @@ import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
 
 @PageTitle("Gastos")
 @Route(value = "Gastos", layout = MainLayout.class)
+@StyleSheet("./estilos/gastos.css")
 public class GastosView extends VerticalLayout {
 
     public GastosView() {
@@ -22,10 +26,26 @@ public class GastosView extends VerticalLayout {
         if (analise == null){
             UI.getCurrent().navigate("");
         }else{
-        String texto = analise.getTitulo() + "|" + analise.getFaturamento();
-        add(new Paragraph(texto));
+            HorizontalLayout containerFormulario = new HorizontalLayout();
+            containerFormulario.addClassName("container");
+            VerticalLayout containerResultado = new VerticalLayout();
+            containerResultado.addClassName("container");
+            HorizontalLayout containerBotoes = new HorizontalLayout();
+            containerBotoes.addClassName("container");
+
+            FormLayout formLayout = new FormLayout();
+            formLayout = formulario(formLayout);
+            containerFormulario.add(formLayout);
+            
+            add(containerFormulario, containerResultado, containerBotoes);
         
         } 
     }
+
+    private FormLayout formulario(FormLayout formLayout){
+
+        return formLayout;
+    }
+
 
 }
