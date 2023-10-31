296b211b4267109fce7dfcc963e9b18852a9cbc7
Lucimar Barros <lucimar7324@hotmail.com>
2023-10-31 00:15:57 -0300

    estilização sobre

--- src/main/java/com/example/application/views/sobre/SobreView.java
+++ src/main/java/com/example/application/views/sobre/SobreView.java
@@ -55,19 +55,19 @@ public class SobreView extends VerticalLayout {
 
     private VerticalLayout sobreProjeto(VerticalLayout container){
         H2 tituloAnalisador = new H2("O Analisador de Gastos");
-        Text textoAnalisador = new Text("É uma aplicação intuitiva e eficiente projetada para simplificar o processo de registro, \r\r\n" + //
+        Paragraph textoAnalisador = new Paragraph("É uma aplicação intuitiva e eficiente projetada para simplificar o processo de registro, \r\r\n" + //
                 "categorização e análise de despesas financeiras pessoais ou comerciais. Essa ferramenta \r\r\n" + //
                 "versátil oferece recursos essenciais para auxiliar os usuários a manter o controle de seus \r\n" + //
-                "gastos, \rcategorizá-los e gerar relatórios detalhados em formato CSV.");
+                "gastos, categorizá-los e gerar relatórios detalhados em formato CSV.");
         H2 tituloApp = new H2("É uma aplicação feita");
-        Text textoApp = new Text("Por alunos da Universidade Estácio, que estão cursando Análise e Desenvolvimento de \r\r\n" + // 
+        Paragraph textoApp = new Paragraph("Por alunos da Universidade Estácio, que estão cursando Análise e Desenvolvimento de \r\r\n" + // 
                 "Sistemas e Ciências da Computação. Eles estavam estudando a disciplina de Programação \r\r\n" + //
                 "Orientada a Objetos em Java, ministrada pelo professor Almir Rogério de Macedo.");
-        Text textoApp2 = new Text("Para a aluna Lara Guimarães, estudante de Ciências Contábeis, que desempenhou o papel \r\r\n" + //
+        Paragraph textoApp2 = new Paragraph("Para a aluna Lara Guimarães, estudante de Ciências Contábeis, que desempenhou o papel \r\r\n" + //
                 "de explicar como a aplicação deveria funcionar e acompanhar o progresso do projeto para \r\n" + //
                 "garantir que estivesse alinhado com suas expectativas.");
         H2 tituloAluno = new H2("De alunos para alunos");
-        Text textoAluno = new Text("Desenvolvida utilizando técnicas que aprendemos em outros semestres durante o curso,\r\r\n" + //
+        Paragraph textoAluno = new Paragraph("Desenvolvida utilizando técnicas que aprendemos em outros semestres durante o curso,\r\r\n" + //
                 "como Kanban, metodologia RAD, GIT, entre outros. Feita durante o segundo semestre de\r\n" + //
                 "2023.");
         container.add(tituloAnalisador, textoAnalisador, tituloApp, textoApp, textoApp2, tituloAluno, textoAluno);            
@@ -79,6 +79,7 @@ public class SobreView extends VerticalLayout {
         container.add(tituloAutores);
         
         VerticalLayout alunos = new VerticalLayout();
+    
         alunos.addClassName("container");
         
         VerticalLayout fabricio = new VerticalLayout();
@@ -89,39 +90,40 @@ public class SobreView extends VerticalLayout {
             " Líder e Documentação"
             );
         alunos.add(fabricio);
-
+        fabricio.addClassName("autores");
         VerticalLayout lucimar = new VerticalLayout();
         lucimar = campoAutor(lucimar,
             " Lucimar Barros de Oliveira",
             " 202109086758",
             " Análise e desenvolvimento de sistemas",
-            " Backend / Frontend"
+            " Backend e Frontend"
             );
         alunos.add(lucimar);
-
-        VerticalLayout Nicolas = new VerticalLayout();
-        Nicolas = campoAutor(Nicolas,
+        lucimar.addClassName("autores");
+        VerticalLayout nicolas = new VerticalLayout();
+        nicolas = campoAutor(nicolas,
             " Nícolas Moniz Rodrigues.",
             "  201902180259",
             " Ciências da computação",
             " Backend"
             );
-        alunos.add(Nicolas);
-
-        VerticalLayout Bruno = new VerticalLayout();
-        Bruno = campoAutor(Bruno,
+        alunos.add(nicolas);
+        nicolas.addClassName("autores");
+        VerticalLayout bruno = new VerticalLayout();
+        bruno = campoAutor(bruno,
             " Bruno Santana de Sousa.",
             " 202204064031",
             " Análise e desenvolvimento de sistemas",
             " Front-end e Design"
             );
-        alunos.add(Bruno);
+        alunos.add(bruno);
+        bruno.addClassName("autores");
         container.add(alunos);
+        container.getElement().getThemeList().clear();
         return container;
     }
 
     private VerticalLayout campoAutor(VerticalLayout container, String nomeAutor, String matriculaAutor, String cursoAutor, String responsabilidadeAutor){
-
         
         Span nome = new Span("Nome:");
         nome.addClassName("negrito");
@@ -155,7 +157,7 @@ public class SobreView extends VerticalLayout {
             containerCurso,
             containerResponsabilidade
         );
-        
+        container.getElement().getThemeList().clear();
         return container;
     }
 
